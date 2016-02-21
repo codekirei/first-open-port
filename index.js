92a5c1cac35e0dd4dc2f26bbbe3fdd594c3e6b38
Jacob Blakely <jacob@codekirei.com>
2016-02-21 13:33:26 -0800

    improve syntax a bit

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const net = require('net')
 const Promise = require('bluebird')
 
 function firstOpenPort(port, max) {
-  const start = port
   max = max || 65535
+  const err = `no ports open from ${port} to ${max}`
 
-  return new Promise((res, rej) => {
+  return new Promise((res, rej) =>
     (function test() {
       const server = net.createServer()
       server.on('error', () => {
-        if ((port += 1) <= max) test(port)
-        else rej(new Error(`no ports open from ${start} to ${max}`))
+        if ((port += 1) <= max) test()
+        else rej(new Error(err))
       })
-      server.on('listening', () => server.close())
+      server.on('listening', server.close)
       server.on('close', () => res(port))
       server.listen(port)
     })()
-  })
+  )
 }
 
 module.exports = firstOpenPort
